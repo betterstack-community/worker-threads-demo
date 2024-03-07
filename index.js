c43ce2acbb24c3864a88afdf7b7ca02e607a5d0e
Stanley Ulili <stanleyulili@gmail.com>
2024-03-07 16:13:24 +0200

    add the starting program

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/non-blocking", (req, res) => {
+  res.send("This is a non-blocking endpoint");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
