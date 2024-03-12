cc5d556685eb14fc9137e1ca28c5e29c860cb9ca
Ayooluwa Isaiah <ayisaiah@gmail.com>
2024-03-12 18:48:27 +0100

    format with prettier

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/non-blocking", (req, res) => {
-  res.send("This is a non-blocking endpoint");
+app.get('/non-blocking', (req, res) => {
+  res.send('This is a non-blocking endpoint');
 });
 
 app.listen(PORT, () => {
